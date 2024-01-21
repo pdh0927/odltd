ef8c5ec3dc9a4e2014ed2d30e4c411416dd036fe
dh <pdh980927@naver.com>
2024-01-21 10:44:51 +0900

    feat: 공휴일 일 때, 빨간색 표시

--- lib/calendar/view/custom_calendar_screen.dart
+++ lib/calendar/view/custom_calendar_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:odltd/calendar/const/holidays_data.dart';
 
 class CustomCalendarScreen extends StatefulWidget {
   const CustomCalendarScreen({super.key});
@@ -123,6 +124,7 @@ class CalendarGrid extends StatelessWidget {
   }
 }
 
+// DayTile 클래스
 class DayTile extends StatelessWidget {
   final DateTime day;
   final DateTime currentDate;
@@ -132,21 +134,58 @@ class DayTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(2.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(4.0),
-      ),
-      child: Center(
-        child: Text(
-          DateFormat('d').format(day),
-          style: TextStyle(
-            fontSize: 16.0,
-            color: currentDate.month == day.month ? Colors.black : Colors.grey,
+    final isSunday = day.weekday == DateTime.sunday;
+    final isHolidayDay = isHoliday(day);
+    final isToday = DateTime.now().day == day.day &&
+        DateTime.now().month == day.month &&
+        DateTime.now().year == day.year;
+
+    final textColor = (isSunday || isHolidayDay) ? Colors.red : Colors.black;
+
+    return InkWell(
+      onTap: () {
+        // 날짜별 일정 상세 보기 기능 구현 (예: 팝업 또는 다이얼로그 표시)
+      },
+      child: Container(
+        margin: const EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+          color: isToday ? Colors.blue[100] : Colors.white, // 오늘 날짜 강조
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Center(
+          child: Text(
+            DateFormat('d').format(day),
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: currentDate.month == day.month ? textColor : Colors.grey,
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+// 공휴일 확인을 위한 함수
+bool isHoliday(DateTime date) {
+  // 매년 동일한 공휴일 확인
+  for (var holiday in commonHolidays) {
+    if (holiday.date.month == date.month && holiday.date.day == date.day) {
+      return true;
+    }
+  }
+
+  // 해당 연도의 공휴일 확인
+  var yearHolidays = yearlyHolidays[date.year];
+  if (yearHolidays != null) {
+    for (var holiday in yearHolidays) {
+      if (holiday.date.month == date.month && holiday.date.day == date.day) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
